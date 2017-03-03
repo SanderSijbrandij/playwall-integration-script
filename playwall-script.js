8f68541643eeb118188855a257ff5221bc09e844
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-03 10:28:53 +0100

    clean js

--- playwall-script.js
+++ playwall-script.js
@@ -1,3 +1,5 @@
+window.addEventListener("message", receiveMessage, false);
+
 function modalOpened() {
   $('body').addClass('playwall-modal-open');
   $('#playwall-frame').addClass('modal-open');
@@ -7,16 +9,15 @@ function modalClosed() {
   $('#playwall-frame').removeClass('modal-open');
 }
 
+function receiveMessage(event)
+{
+  if (event.data == 'closeModal') {
+    // your own cleanUp requirements
+    modalClosed();
+  }
+}
+
 $(document).ready(function() {
   // your own checks for requiring the paywall
   modalOpened();
 });
-
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event)
-{
-  if (event.data == 'closeModal') {
-    modalClosed();
-  }
-}
